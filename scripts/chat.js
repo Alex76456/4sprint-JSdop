7926a2b106a0cea413d0e1a3912c2d8e366cfb4b
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2020-10-31 19:39:16 +0300

    feat: доделать все страницы

--- scripts/chat.js
+++ scripts/chat.js
@@ -4,10 +4,6 @@ const popup = chat.querySelector('.chat__popup');
 
 const openButton = chat.querySelector('.chat__button');
 
-
-
-
-
 function showPopup() {
   if (popup.classList.contains('chat__popup_opened')) {
     popup.classList.remove('chat__popup_opened');
@@ -17,48 +13,20 @@ function showPopup() {
 }
 
 
-
-
-
-
 const message = chat.querySelector('.chat__popup-messages');
 
-const messageInput = chat.querySelector('.chat__button');
+const messageInput = chat.querySelector('.chat__popup-input');
 
 const chatForm = chat.querySelector('.chat__popup-form');
 
-
-function submitForm(event) {
-  if (event.keyCode === 13) {
-    message.insertAdjacentHTML('beforeend', messageInput.value);
-    messageInput.value = '';
-  }
-
-};
-
-openButton.addEventListener('click', showPopup);
-chatForm.addEventListener('submit', submitForm);
-
-/*
-
-// Выполнение функции, когда пользователь отпускает клавишу на клавиатуре
-input.addEventListener("keyup", function(event) {
-  // Число 13 в "Enter" и клавиши на клавиатуре
-  if (event.keyCode === 13) {
-    // При необходимости отмените действие по умолчанию
-    event.preventDefault();
-    // Вызов элемента button одним щелчком мыши
-    document.getElementById("myBtn").click();
-  }
-});
-
-
-/*
+let string = '\n \n'
 
 function submitForm(event) {
   event.preventDefault();
-  message.textContent = message.textContent + messageInput.value;
-  messageInput.textContent = '';
+  message.textContent = message.textContent + string + messageInput.value;
+  messageInput.value = '';
 };
 
-*/
+
+openButton.addEventListener('click', showPopup);
+chatForm.addEventListener('submit', submitForm);
